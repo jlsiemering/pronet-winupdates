67994eb1245d111e3178bd66e78d5de8b7ff7d84
Jordan <86007705+jlsiemering@users.noreply.github.com>
2021-06-16 12:09:50 -0500

    Delete windows_update_for_business_config.ps1

--- windows_update_for_business_config.ps1
+++
@@ -1,6 +0,0 @@
-#Set group policy objects for windows update for business. This will probably only work for Windows 10
-
-#install group policy management tools
-Add-WindowsCapability -Online -Name Rsat.GroupPolicy.Management.Tools~~~~0.0.1.0
-
-
